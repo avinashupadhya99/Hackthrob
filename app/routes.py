75b93022cfff74ee8a6ae4a666cb0dcb9a56d3f9
unknown <avinashupadhya99@gmail.com>
2021-02-13 16:31:59 +0530

    Implemented users model

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
 from app import app
+from app.database import get_users
+from flask import jsonify
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/users')
+def users():
+    users = get_users()
+
+    return jsonify(users)
